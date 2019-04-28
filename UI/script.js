691a11e3d2388ca81f1e8ce648039228c59c261c
Dmitriy <dmitriy_metelskiy@mail.ru>
2019-04-29 00:42:33 +0300

    post buttons available and ready to use

--- UI/script.js
+++ UI/script.js
@@ -193,8 +193,8 @@ class Controller {
         this._view._postsWrapper.addEventListener('click', Controller._onPostClick);
     }
 
-    _showPosts(skip = 0, top = 10, filter = {}) {
-        this._view._showPosts(this._photoList._getPosts(skip, top, filter/*filter from filter form*/));
+    static _showPosts(skip = 0, top = 10, filter = {}) {
+        controller._view._showPosts(controller._photoList._getPosts(skip, top, filter/*filter from filter form*/));
     }
 
     _showMorePosts() {
@@ -216,15 +216,6 @@ class Controller {
         }
         else {
             alert('Failed to edit post.');
-        }
-    }
-    
-    _removePost() {
-        if(this._photoList._remove(event.currentTarget.parentNode.parentNode.parentNode.parentNode.id)) {
-            this._view._showPosts(this._photoList._getPosts());
-        }
-        else {
-            alert('Failed to remove post.');
         }
     }
 
@@ -251,16 +242,27 @@ class Controller {
             const id = button.parentNode.parentNode.parentNode.id;
             const post = controller._photoList._get(id);
 
+            form.id = id.substring(1);
+
             const image = form.querySelector('.edit-form__image').firstElementChild;
             image.setAttribute('src', post.photoLink);
             form.querySelector('.edit-form__author').firstChild.textContent = post.author;
-            form.querySelector('#edit-image-url').value = post.photoLink;
-            form.querySelector('#edit-hashTags').value = post.hashTags;
+            const inputImage = form.querySelector('#edit-image-url');
+            inputImage.value = post.photoLink;
+            form.querySelector('#edit-hashTags').value = '#' + post.hashTags.join(' #');
             form.querySelector('#edit-description').value = post.description;
 
         }
         else if(button.className === 'post__delete-button') {   // Обработка клика на delete
-            alert('Delete clicked');
+            if(confirm('Are you sure you want to delete post?')) {
+                const postElement = button.parentNode.parentNode.parentNode;
+                if(controller._photoList._remove(postElement.id)) {
+                    postElement.parentNode.removeChild(postElement);
+                }
+                else {
+                    alert('Failed to remove post.');
+                }
+            }
         }
     }
 
@@ -269,97 +271,63 @@ class Controller {
         controller._formWrapper.querySelector('.edit-form').style.display = 'none';
     }
 
-    _login(username) {
-        this._view._user = username;
-        this._view._showHeader();
-        this._view._clearPosts();
-        this._showPosts();
+    static _setEditImage() {
+        console.log(controller._formWrapper.querySelector('.edit-form__image'));
+        const img = controller._formWrapper.querySelector('.edit-form__image').firstElementChild;
+        img.setAttribute('src', controller._formWrapper.querySelector('#edit-image-url').value);
     }
 
-    _logout() {
-        this._view._logout();
-        this._showPosts();
+    static _submitEditForm() {
+        const editForm = event.target.parentNode.parentNode.parentNode;
+        const postObject = {};
+        postObject.photoLink = editForm.querySelector('#edit-image-url').value;
+        postObject.description = editForm.querySelector('#edit-description').value;
+        postObject.hashTags = editForm.querySelector('#edit-hashTags').value.trim().substring(1).split(/[ #]+/);
+
+        alert(postObject.description + '\n#' + postObject.hashTags.join(' #'));
+
+        if(controller._photoList._edit(`_${editForm.id}`, postObject)) {
+            controller._view._editPost(`_${editForm.id}`, postObject);
+            editForm.style.display = 'none';
+            controller._formWrapper.style.display = 'none';
+        }
+        else {
+            alert('Failed to edit post.');
+        }
+    }
+
+    static _login(username) {
+        controller._view._user = username;
+        controller._view._showHeader();
+        controller._view._clearPosts();
+        Controller._showPosts();
+    }
+
+    static _logout() {
+        controller._view._logout();
+        controller._view._clearPosts();
+        Controller._showPosts();
     }
 }
 
 const controller = new Controller();
-controller._showPosts();
+Controller._showPosts();
 
 (function foo() {
-    const editCloseBtn = controller._formWrapper.querySelector('.edit-form__close-button');
+    const logOut = document.querySelector('.header__user').lastElementChild;
+    logOut.addEventListener('click', Controller._logout);
+
+    const headerHome = document.querySelector('.header__home');
+    headerHome.firstElementChild.addEventListener('click', Controller._showPosts);
+
+    const editForm = controller._formWrapper.querySelector('.edit-form');
+
+    const editCloseBtn = editForm.querySelector('.edit-form__close-button');
     editCloseBtn.addEventListener('click', Controller._closeEditForm);
+
+    const inputURL = editForm.querySelector('#edit-image-url');
+    inputURL.addEventListener('change', Controller._setEditImage);
+
+    const editSubmit = editForm.querySelector('.edit-form__submit');
+    editSubmit.addEventListener('click', Controller._submitEditForm);
 }())
-
-var filter1 = { // 15, 4 & 2
-    author: 'Marilyn Monroe'
-}
-var filter2 = { // 20 & 14
-    author: 'Abraham Lincoln',
-    hashTags: ['president'],
-    fromDate: new Date(1810, 1, 1),
-    tillDate: new Date(1840, 12, 31)
-}
-var filter3 = { // 20, 14 & 6
-    author: 'Abraham Lincoln',
-    hashTags: ['president']
-}
-var filter4 = { // 11
-    hashTags: ['helicopter', 'engineer'],
-    fromDate: new Date(1450, 1, 1),
-    tillDate: new Date(1600, 12, 31)
-}
-var filter5 = { // []
-    hashTags: ['masterpiece'],    
-    fromDate: new Date(1500, 1, 1),
-    tillDate: new Date(1600, 12, 31)
-}
-var filter6 = { // 10, 21(not added), 20, 14, 18, 16, 8, 6, 17, 11, 13
-    fromDate: new Date(1400, 1, 1),
-    tillDate: new Date(1900, 12, 31)
-}
-    
-var newPost1 = {
-        id: '21',
-        description: 'Vincent Willem van Gogh.',
-        createdAt: new Date('1853-03-30T19:15:26'),
-        author: 'Vincent Van Gogh',
-        photoLink: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/220px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg',
-        hashTags: ['picture'],
-        likes: []
-    }
-var newPost2 = {
-        id: '22',
-        description: 'Vincent Willem van Gone.',
-        createdAt: new Date('2018-03-18T19:57:26'),
-        author: 'Vincent Van Gogh',
-        photoLink: 'https://imgc.allpostersimages.com/img/print/posters/vincent-van-gogh-selfie-portrait_a-G-10455983-0.jpg',
-        hashTags: ['picture', 'meme'],
-        likes: []
-    }
-var newPost3 = {
-    id: '25',
-    description: 'Newly added post[3].',
-    createdAt: new Date('1839-02-12T14:23:07'),
-    author: 'Abba',
-    photoLink: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Emancipation_proclamation.jpg/1024px-Emancipation_proclamation.jpg',
-    hashTags: ['president', 'noslavery'],
-    likes: []
-}
-var newPost4 = {
-    id: '28',
-    description: 'Newly added post[4]',
-    createdAt: new Date('2007-03-12T17:22:07'),
-    author: 'Abba',
-    photoLink: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Emancipation_proclamation.jpg/1024px-Emancipation_proclamation.jpg',
-    hashTags: ['new', 'brand_new'],
-    likes: []
-}
-
-var editor1 = {
-    description: [],
-    photoLink: 'http://haradok.info/static/news/5/4565/preview.jpg'
-}
-var editor2 = {
-    author: 'abcda',
-    hashTags: ['new like', 'edited like']
-}
